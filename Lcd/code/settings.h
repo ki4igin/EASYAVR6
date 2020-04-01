435a383ea2bf400346b1fde9927c3eaa801066eb
artem <ki4igin1992@outlook.com>
2020-04-01 19:31:04 +0300

    Init Lcd Project

---
+++ Lcd/code/settings.h
@@ -0,0 +1,12 @@
+#ifndef __SETTINGS_H
+#define __SETTINGS_H
+
+// Macro -----------------------------------------------------------------------
+#ifndef __AVR_ATmega16__
+#define __AVR_ATmega16__
+#endif
+
+#define F_CPU 8000000UL
+
+#endif
+// End File --------------------------------------------------------------------
