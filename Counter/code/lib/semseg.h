ae2665f49123e0883563d0b2c77eca1835f4f3de
artem <ki4igin1992@outlook.com>
2021-03-01 23:10:03 +0300

    Рефакторинг

--- Counter/code/lib/semseg.h
+++ Counter/code/lib/semseg.h
@@ -2,10 +2,9 @@
 #define __SEMSEG_H
 
 // Includes --------------------------------------------------------------------
-#include <avr/io.h>        // Заголовочный файл для работы с РВВ МК
-#include <avr/pgmspace.h>  // Заголовчный файл для работы с памятью программ МК
+#include <avr/io.h>  // Заголовочный файл для работы с РВВ МК
 
-// Function prototypes ---------------------------------------------------------
+// Function Prototypes ---------------------------------------------------------
 void SemsegInit(void);
 void SemsegBin2Bcd(uint16_t data, uint8_t* buf, uint8_t bufSize);
 void SemsegDisp(uint8_t* buf, uint8_t bufSize);
